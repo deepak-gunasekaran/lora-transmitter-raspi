a2b75dd57bf3a379d732466c54789833c2406f30
Deepak <dpk.com@gmail.com>
2022-12-28 22:09:29 -0600

    Cleanup

--- transmitter.py
+++ transmitter.py
@@ -2,9 +2,6 @@ import time
 import serial
 import random
 
-word_file = "words"
-WORDS = open(word_file).read().splitlines()
-final=random.choice(WORDS)
 lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 
 while True:
@@ -14,4 +11,4 @@ while True:
     b = bytes(final,'utf-8')
     s = lora.write(b)#send the data to other lora
     print (final)
-    time.sleep(40)#delay of 200ms
+    time.sleep(40)#delay of 40s
